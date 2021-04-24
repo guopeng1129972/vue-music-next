2a751a67bc364b729565780ef1937af5da4cc4db
guopeng1129972 <381694179@qq.com>
2021-04-24 18:23:59 +0800

    2-7 轮播图组件的开发

---
+++ src/components/base/use-slider.js
@@ -0,0 +1,30 @@
+import BScoll from "@better-scroll/core";
+import Slide from "@better-scroll/slide";
+
+import { onMounted, onUnmounted, ref } from "vue";
+BScoll.use(Slide);
+export function useSlider(wrapperRef) {
+  const slider = ref(null);
+  const currentPageIndex = ref(0);
+  onMounted(() => {
+    const sliderVal = (slider.value = new BScoll(wrapperRef.value, {
+      click: true,
+      scrollX: true,
+      scrollY: false,
+      momentum: false,
+      bounce: false,
+      probeType: 2,
+      slide: true,
+    }));
+    sliderVal.on("slideWillChange", (page) => {
+      currentPageIndex.value = page.x;
+    });
+  });
+  onUnmounted(() => {
+    slider.value.destroy();
+  });
+  return {
+    slider,
+    currentPageIndex
+  };
+}

2647ca2f0231c6f2b1bf1e3f8e4dc3e39d9e27d9
guopeng1129972 <381694179@qq.com>
2021-07-21 22:56:35 +0800

    5-2 5-3 播放器歌曲前进与后退逻辑

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -11,6 +11,25 @@
         <h1 class="title">{{ currentSong.name }}</h1>
         <h2 class="subtitle">{{ currentSong.singer }}</h2>
       </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i @click="prev" class="icon-prev"></i>
+          </div>
+          <div @pause="pause" class="icon i-center">
+            <i @click="togglePlay" :class="playIcon"></i>
+          </div>
+          <div class="icon i-right">
+            <i @click="next" class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"> </i>
+          </div>
+        </div>
+      </div>
       <audio ref="audioRef"></audio>
     </div>
   </div>
@@ -27,6 +46,14 @@ export default {
     const fullScreen = computed(() => store.state.fullScreen);
     // 注意获取的时候是getters
     const currentSong = computed(() => store.getters.currentSong);
+    // 获取播放状态
+    const playing = computed(() => store.state.playing);
+    const playlist = computed(() => store.state.playlist);
+    const playIcon = computed(() => {
+      return playing.value ? "icon-pause" : "icon-play";
+    });
+    // 获取当前歌曲
+    const currentIndex = computed(() => store.state.currentIndex);
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return;
@@ -35,14 +62,73 @@ export default {
       audioEl.src = newSong.url;
       audioEl.play();
     });
+    watch(playing, (newPlaying) => {
+      const audioEl = audioRef.value;
+      newPlaying ? audioEl.play() : audioEl.pause();
+    });
+    watch(currentIndex, (newCurrentIndex) => {
+      const audioEl = audioRef.value;
+      newCurrentIndex;
+      audioEl.play();
+    });
     function goback() {
       store.commit("setFullScreen", false);
+    }
+    function togglePlay() {
+      store.commit("setPlayingState", !playing.value);
+    }
+    function pause() {
+      store.commit("setPlayingState", false);
+    }
+    function next() {
+      const list = playlist.value;
+      if (!list.length) {
+        return;
+      }
+      if (list.length === 1) {
+        loop();
+      } else {
+        let index =
+          currentIndex.value + 1 === list.length ? 0 : currentIndex.value + 1;
+        store.commit("setCurrentIndex", index);
+        if (!playing.value) {
+          store.commit("setPlayingState", true);
+        }
+      }
+    }
+    function prev() {
+      const list = playlist.value;
+      if (!list.length) {
+        return;
+      }
+      if (list.length === 1) {
+        loop();
+      } else {
+        let index =
+          currentIndex.value - 1 === -1
+            ? list.length - 1
+            : currentIndex.value - 1;
+        store.commit("setCurrentIndex", index);
+        if (!playing.value) {
+          store.commit("setPlayingState", true);
+        }
+      }
+    }
+    function loop() {
+      const audioEl = audioRef.value;
+      audioEl.currentTime = 0;
+      audioEl.play();
     }
     return {
       currentSong,
       fullScreen,
       audioRef,
       goback,
+      playIcon,
+      togglePlay,
+      pause,
+      next,
+      prev,
     };
   },
 };
@@ -102,6 +188,41 @@ export default {
         text-align: center;
         font-size: $font-size-medium;
         color: $color-text;
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
       }
     }
   }
